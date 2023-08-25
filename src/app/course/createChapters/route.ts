b141eb30a6f9a3e11d09b09b3d6267b7308e112d
Echo Wang <ywang623@my.bcit.ca>
2023-08-25 13:52:45 -0700

    Added the course generation API handling for OpenAI and unsplash API

---
+++ src/app/course/createChapters/route.ts
@@ -0,0 +1,74 @@
+import { NextResponse } from "next/server";
+import { createChapterSchema } from "@/validators/course";
+import { ZodError } from "zod";
+import { strict_output } from "@/lib/gpt";
+import { getUnsplashImage } from "@/lib/unsplash";
+import { prisma } from "@/lib/db";
+
+export async function POST(req: Request, res: Response) {
+  try {
+    const body = await req.json();
+    const { title, units } = createChapterSchema.parse(body);
+
+    type outputUnits = {
+      title: string;
+      chapters: {
+        youtube_search_query: string;
+        chapter_title: string;
+      }[];
+    }[];
+
+    let output_units: outputUnits = await strict_output(
+      "You are an AI capable of creating course content, coming up with relavent chapter titles, and finding relavent youtube videos for each chapter",
+      new Array(units.length).fill(
+        `It is your job to create a course about ${title}. The user has requested to create chapters for each of the units. Then, for each chapter, provide a detailed youtube search query that can be used to find an imformative educational video that is relavent to the chapter. Each query should give an educational informative course in youtube.`
+      ),
+      {
+        title: "title of the unit",
+        chapters:
+          "an array of chapters, each chapter should have a youtube_search_query and a chapter_title key in the json object",
+      }
+    );
+    const imageSearchTerm = await strict_output(
+      "You are an AI capable of finding the most relavent image for a course",
+      `Plase provide a good image search term for the title of a course about ${title}  .This search term will be fed into the unsplash api, so make sure it is a good search term for an image.`,
+      {
+        image_search_term: "a good search term for the title of the course",
+      }
+    );
+    const course_image = await getUnsplashImage(
+      imageSearchTerm.image_search_term
+    );
+    const course = await prisma.course.create({
+      data: {
+        name: title,
+        image: course_image,
+      },
+    });
+
+    for (const unit of output_units) {
+      const title = unit.title;
+      const prismaUnit = await prisma.unit.create({
+        data: {
+          name: title,
+          courseId: course.id,
+        },
+      });
+      await prisma.chapter.createMany({
+        data: unit.chapters.map((chapter) => {
+          return {
+            name: chapter.chapter_title,
+            unitId: prismaUnit.id,
+            youtubeSearchQuery: chapter.youtube_search_query,
+          };
+        }),
+      });
+    }
+
+    return NextResponse.json({ course_id: course.id });
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return new NextResponse("invalid body", { status: 400 });
+    }
+  }
+}
---
+++ src/lib/unsplash.ts
@@ -0,0 +1,6 @@
+import axios from "axios";
+export const getUnsplashImage = async (query: string) => {
+  const { data } = await axios.get(`
+    https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.UNSPLASH_API_KEY}`);
+  return data.results[0].urls.small_s3;
+};
