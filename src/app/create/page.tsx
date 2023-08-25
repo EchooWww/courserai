82f2234b4fe658ce987a811bf53c8c9903049c45
Echo Wang <ywang623@my.bcit.ca>
2023-08-25 00:06:26 -0700

    Added the create course form

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,9 +1,33 @@
+import CreateCourseForm from "@/components/CreateCourseForm";
+import { getAuthSession } from "@/lib/auth";
+import { InfoIcon } from "lucide-react";
+import { redirect } from "next/navigation";
 import React from "react";
 
 type Props = {};
 
-const CreatePage = (props: Props) => {
-  return <div>page</div>;
+const CreatePage = async (props: Props) => {
+  const session = await getAuthSession();
+  if (!session?.user) {
+    return redirect("./gallery");
+  }
+
+  return (
+    <div className="flex flex-col items-start max-w-xl px-8 mx-auto my-16 sm:px-0">
+      <h1 className="self-center text-3xl font-bold text-center sm:text-6xl">
+        Learning Journey
+      </h1>
+      <div className="flex p-4 mt-5 border-none bg-secondary">
+        <InfoIcon className="w-12 h-12 mr-3 text-blue-400" />
+        <div>
+          Enter a course title, or the topic you wanna learn about. Then enter a
+          list of units, which are the specific aspects you want to learn. And
+          our AI will generate a customized learning journey for you!
+        </div>
+      </div>
+      <CreateCourseForm />
+    </div>
+  );
 };
 
 export default CreatePage;
---
+++ src/components/CreateCourseForm.tsx
@@ -0,0 +1,127 @@
+"use client";
+import React from "react";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form";
+import { z } from "zod";
+import { createChapterSchema } from "@/validators/course";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Input } from "./ui/input";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+import { Plus, Trash } from "lucide-react";
+import { motion, AnimatePresence } from "framer-motion";
+
+type Props = {};
+
+type Input = z.infer<typeof createChapterSchema>;
+
+const CreateCourseForm = (props: Props) => {
+  const form = useForm<Input>({
+    resolver: zodResolver(createChapterSchema),
+    defaultValues: {
+      title: "",
+      units: ["", "", ""],
+    },
+  });
+  function onSubmit(data: Input) {
+    console.log(data);
+  }
+
+  return (
+    <div className="w-full">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full mt-4">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => {
+              return (
+                <FormItem className="flex flex-col items-start w-full sm:items-center sm:flex-row">
+                  <FormLabel className="flex-[1] text-xl">Title</FormLabel>
+                  <FormControl className="flex-[6]">
+                    <Input
+                      placeholder="Enter the main topic of the course"
+                      {...field}
+                    ></Input>
+                  </FormControl>
+                </FormItem>
+              );
+            }}
+          />
+
+          <AnimatePresence>
+            {form.watch("units").map((_, index) => {
+              return (
+                <motion.div
+                  key={index}
+                  initial={{ opacity: 0, height: 0 }}
+                  animate={{ opacity: 1, height: "auto" }}
+                  exit={{ opacity: 0, height: 0 }}
+                  transition={{
+                    opacity: { duration: 0.2 },
+                    height: { duration: 0.2 },
+                  }}
+                >
+                  <FormField
+                    key={index}
+                    control={form.control}
+                    name={`units.${index}`}
+                    render={({ field }) => {
+                      return (
+                        <FormItem className="flex flex-col items-start w-full sm:items-center sm:flex-row">
+                          <FormLabel className="flex-[1] text-xl">
+                            Unit {index + 1}
+                          </FormLabel>
+                          <FormControl className="flex-[6]">
+                            <Input
+                              placeholder="Enter the subtopic of the course"
+                              {...field}
+                            ></Input>
+                          </FormControl>
+                        </FormItem>
+                      );
+                    }}
+                  />
+                </motion.div>
+              );
+            })}
+          </AnimatePresence>
+
+          <div className="flex item-center justify-center mt-4">
+            <Separator className="flex-[1]" />
+            <div className="mx-4">
+              <Button
+                type="button"
+                variant="secondary"
+                className="font-semibold"
+                onClick={() => {
+                  form.setValue("units", [...form.getValues().units, ""]);
+                }}
+              >
+                Add Unit
+                <Plus className="w-4 h-4 ml-2 text-green-500" />
+              </Button>
+              <Button
+                type="button"
+                variant="secondary"
+                className="font-semibold ml-2"
+                onClick={() => {
+                  form.setValue("units", form.getValues().units.slice(0, -1));
+                }}
+              >
+                Remove Unit
+                <Trash className="w-4 h-4 ml-2 text-red-500" />
+              </Button>
+            </div>
+            <Separator className="flex-[1]" />
+          </div>
+          <Button type="submit" className="w-full mt-6" size="lg">
+            Let's Go!
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default CreateCourseForm;
